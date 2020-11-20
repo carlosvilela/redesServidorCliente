ee47e453453f35b6588525c615835e3237102a7a
codebreaker <norepply@codebreaker.com.br>
2020-11-20 17:46:57 -0300

    criando os servidores TCP

--- ServidorTCP_log_ex6.py
+++ ServidorTCP_log_ex6.py
@@ -14,7 +14,7 @@ chatServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                
 chatServidor.bind((hostname, porta))                                            # hostname e porta de acesso
 chatServidor.listen(30)                                                         # numero de Conexões permitidas
 
-# Função para calcular o valor adicionando 11%
+# Função para calcular o logaritmo na base 10 de um valor
 def logaritmo (valor):
     calculo = math.log10(valor)
     return calculo
@@ -25,9 +25,9 @@ try:                                                                        # tr
         clienteSocket, enderecoCliente = chatServidor.accept()              # Espera por conexão do cliente
         mensagem = clienteSocket.recv(tamanhoBuffer)                        # recebe a mensagem do Cliente
         mensagem = mensagem.decode("ascii")                                 # converte os bytes na tabela ascii
-        valor = float(mensagem)                                               # converte a string em numero inteiro
-        calc = logaritmo(valor)                                                  # realiza os calculos
-        mensagem = str(calc).encode("utf-8")                                 # converte o numero inteiro em string
+        valor = float(mensagem)                                             # converte a string em numero inteiro
+        calc = logaritmo(valor)                                             # realiza os calculos
+        mensagem = str(calc).encode("utf-8")                                # converte o numero inteiro em string
         clienteSocket.send(mensagem)                                        # retorna a mensagem processada ao cliente
 
 except KeyboardInterrupt:                                                   # caso haja erro irá retornar uma ação
