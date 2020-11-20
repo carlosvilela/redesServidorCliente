a325ef962464fc657001ac873da790262f5a8b53
codebreaker <norepply@codebreaker.com.br>
2020-11-20 17:17:16 -0300

    criando os servidores TCP

---
+++ ServidorTCP_fatorial.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# importando bibliotecas
+import socket
+
+# Variaveis
+hostname = "localhost"                                                          # endereço local
+porta = 1404                                                                    # porta de comunicação
+tamanhoBuffer = 4*(1024)                                                        # Tamanho do Buffer
+
+# Criando Sockets TCP em IPv6
+chatServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                # Cria Socket TCP
+chatServidor.bind((hostname, porta))                                            # hostname e porta de acesso
+chatServidor.listen(30)                                                         # numero de Conexões permitidas
+
+# Função para calcular o fatorial de um numero
+def fatorial (numeroInteiro):
+    tamanho = numeroInteiro + 1
+    calculo = 1
+    for i in range(1,tamanho):
+        calculo = calculo * i
+    return calculo
+
+# Loop infinito, aguardando requisições
+try:                                                                        # tratamento de erros
+    while True:
+        clienteSocket, enderecoCliente = chatServidor.accept()              # Espera por conexão do cliente
+        mensagem = clienteSocket.recv(tamanhoBuffer)                        # recebe a mensagem do Cliente
+        mensagem = mensagem.decode("ascii")                                 # converte os bytes na tabela ascii
+        valor = int(mensagem)                                               # converte a string em numero inteiro
+        fat = fatorial(valor)                                               # realiza os calculos
+        mensagem = str(fat).encode("utf-8")                                 # converte o numero inteiro em string
+        clienteSocket.send(mensagem)                                        # retorna a mensagem processada ao cliente
+
+except KeyboardInterrupt:                                                   # caso haja erro irá retornar uma ação
+    chatCliente.close()
---
+++ ServidorTCP_incorporar11.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+# importando bibliotecas
+import socket
+
+# Variaveis
+hostname = "localhost"                                                          # endereço local
+porta = 1404                                                                    # porta de comunicação
+tamanhoBuffer = 4*(1024)                                                        # Tamanho do Buffer
+
+# Criando Sockets TCP em IPv6
+chatServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                # Cria Socket TCP
+chatServidor.bind((hostname, porta))                                            # hostname e porta de acesso
+chatServidor.listen(30)                                                         # numero de Conexões permitidas
+
+# Função para calcular o valor adicionando 11%
+def incorporar (valor):
+    porcentagem = 1 + (11/100)
+    calculo = valor*porcentagem
+    return calculo
+
+# Loop infinito, aguardando requisições
+try:                                                                        # tratamento de erros
+    while True:
+        clienteSocket, enderecoCliente = chatServidor.accept()              # Espera por conexão do cliente
+        mensagem = clienteSocket.recv(tamanhoBuffer)                        # recebe a mensagem do Cliente
+        mensagem = mensagem.decode("ascii")                                 # converte os bytes na tabela ascii
+        valor = float(mensagem)                                               # converte a string em numero inteiro
+        calc = incorporar(valor)                                               # realiza os calculos
+        mensagem = str(calc).encode("utf-8")                                 # converte o numero inteiro em string
+        clienteSocket.send(mensagem)                                        # retorna a mensagem processada ao cliente
+
+except KeyboardInterrupt:                                                   # caso haja erro irá retornar uma ação
+    chatCliente.close()
