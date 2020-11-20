7c0209273c30e20161318c07a9e2b57392621d8e
codebreaker <norepply@codebreaker.com.br>
2020-11-20 18:22:58 -0300

    criando os Clientes TCP

---
+++ Cliente_ex1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# importando bibliotecas
+import socket
+
+# Variaveis
+hostname = "localhost"                                                          # endereço local
+porta = 1404                                                                    # porta de comunicação
+tamanhoBuffer = 4*(1024)                                                        # Tamanho do Buffer
+
+# Criando Sockets TCP em IPv6
+chatCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                 # Cria Socket TCP
+chatCliente.connect((hostname, porta))                                          # Conexão com o Servidor
+
+enviar = input("Digite um valor: ")                                             # entrada de dados
+chatCliente.send(enviar.encode())                                               # envio da mensagem
+
+resultado = chatCliente.recv(1048)                                              # recebe a resposta do servidor
+print("11% acrescido de ",enviar," é = ",resultado)                                 # exibe a resposta do servidor
+
+chatCliente.close()                                                             # finaliza a conexão
